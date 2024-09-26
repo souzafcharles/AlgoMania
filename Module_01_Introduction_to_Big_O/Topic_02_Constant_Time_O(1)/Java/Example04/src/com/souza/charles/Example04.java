5f809687e58119f481e1339e37112fa67a909956
souzafcharles <souzach@outlook.com>
2024-09-26 19:43:51 -0300

    Update Example04

--- Module_01_Introduction_to_Big_O/Topic_02_Constant_Time_O(1)/Java/Example04/src/com/souza/charles/Example04.java
+++ Module_01_Introduction_to_Big_O/Topic_02_Constant_Time_O(1)/Java/Example04/src/com/souza/charles/Example04.java
@@ -11,14 +11,14 @@ import java.util.LinkedList;
 public class Example04 {
 
     public static void main(String[] args) {
-        Queue<Integer> myQueue = new LinkedList<>(); 
+        Queue<Integer> myQueue = new LinkedList<>();
 
-        myQueue.add(1); 
-        myQueue.add(2); 
-        myQueue.add(3); 
+        myQueue.add(1);
+        myQueue.add(2);
+        myQueue.add(3);
 
-        // The size() method is O(1) (constant time) because it returns 
-        // the number of elements in the queue without needing to 
+        // The size() method is O(1) (constant time) because it returns
+        // the number of elements in the queue without needing to
         // traverse the entire data structure.
         System.out.println("Size of the queue: " + myQueue.size());
     }
