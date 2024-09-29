5010a6656b43f6cd0d137c845e17141c08cd6601
souzafcharles <souzach@outlook.com>
2024-09-28 22:07:32 -0300

    Include Java example

---
+++ Module_01_Big_O/Topic_04_Logarithmic_Time_O(log_n)/Java/Example/src/com/souza/charles/Example.java
@@ -0,0 +1,34 @@
+package com.souza.charles;
+/**
+ Course title: AlgoMania
+ Instructor: Thiago Brito - Universo DEV
+ Example adapted by: Charles Fernandes de Souza
+ Date: September 8, 2024
+ */
+
+public class Example {
+    public static Integer binarySearch(int[] myArray, int item) {
+        int lower = 0;
+        int upper = myArray.length - 1;
+        while (lower <= upper) {
+            int middle = (lower + upper) / 2;  // This division step is part of O(log n) complexity, narrowing down the search space.
+            int guess = myArray[middle];
+
+            if (guess == item) {
+                return middle;
+            } else if (guess > item) {
+                upper = middle - 1;  // Reducing the upper bound, this step is O(log n) as it halves the search space.
+            } else {
+                lower = middle + 1;  // Increasing the lower bound, also O(log n) as it halves the search space.
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        int[] myArray = {10, 20, 30, 40};
+        System.out.println(binarySearch(myArray, 30)); //
+    }
+}
+
