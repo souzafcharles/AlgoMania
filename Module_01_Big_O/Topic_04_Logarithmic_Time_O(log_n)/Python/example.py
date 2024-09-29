17224aaf5e7a47c1c3ee8b278fbbd346478fa3b1
Charles Fernandes de Souza <souzach@outlook.com>
2024-09-28 22:26:35 -0300

    Update Python example

--- Module_01_Big_O/Topic_04_Logarithmic_Time_O(log_n)/Python/example.py
+++ Module_01_Big_O/Topic_04_Logarithmic_Time_O(log_n)/Python/example.py
@@ -7,7 +7,6 @@ Date: September 28, 2024
 def binary_search(my_array, item):
     lower_bound = 0
     upper_bound = len(my_array) - 1
-
     while lower_bound <= upper_bound:
         middle_index = (lower_bound + upper_bound) // 2 # It divides the search space in half with each iteration, effectively reducing the search time logarithmically O(log n)
         guess = my_array[middle_index]
@@ -18,7 +17,6 @@ def binary_search(my_array, item):
             upper_bound = middle_index - 1
         else:
             lower_bound = middle_index + 1
-
     return None  #
 
 my_array = [10, 20, 30, 40]
