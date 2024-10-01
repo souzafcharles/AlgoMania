f5b8c33ab846c7bc1555af8b97ec87bcf491c717
Charles Fernandes de Souza <souzach@outlook.com>
2024-10-01 18:31:29 -0300

    Update Example01

--- Module_01_Big_O/Topic_05_Quadratic_Time_O(n²)/Java/Example01/src/com/souza/charles/Example01.java
+++ Module_01_Big_O/Topic_05_Quadratic_Time_O(n²)/Java/Example01/src/com/souza/charles/Example01.java
@@ -1,5 +1,10 @@
 package com.souza.charles;
-
+/**
+ Course title: AlgoMania
+ Instructor: Thiago Brito - Universo DEV
+ Example adapted by: Charles Fernandes de Souza
+ Date: October 01, 2024
+ */
 import java.util.ArrayList;
 
 public class Example01 {
