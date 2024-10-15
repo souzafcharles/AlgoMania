915baa2fb1476f60ccddd6f6653f1b780b9c8e90
Charles Fernandes de Souza <souzach@outlook.com>
2024-10-15 11:37:15 -0300

    Topic 07 Factorial Time O(n!)

--- Module_01_Big_O/Topic_07_Factorial_Time_O(n!)/Java/Example01/src/com/souza/charles/Example01.java
+++ Module_01_Big_O/Topic_07_Factorial_Time_O(n!)/Java/Example01/src/com/souza/charles/Example01.java
@@ -11,13 +11,13 @@ public class Example01 {
         for (int each = 0; each < maxNumber; each++) {
             System.out.println(each);
             // Recursively calls the factorial function, decreasing the maxNumber by 1 each time
-            // This recursive call generates factorial growth (O(n!)) due to the nature
+            // This recursive call generates factorial growth O(n!) due to the nature
             // of multiple recursive invocations
             factorial(maxNumber - 1);
         }
     }
 
     public static void main(String[] args) {
-        factorial(3);
+        factorial(5);
     }
 }
